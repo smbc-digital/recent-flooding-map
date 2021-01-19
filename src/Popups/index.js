e49a005d8b253a6feb39e7ba9ac26725d91e2561
Daniel Kinder <daniel.kinder@stockport.gov.uk>
2021-01-19 18:27:49 +0000

    Popups

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,7 +1,7 @@
 const livefloodingincidentsPopup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-tint" aria-hidden="true"></i><p class="title">${feature.properties.name}</p>
+  const content = `<div class="item"><i class="fa fa-tint" aria-hidden="true"></i><p class="title">${feature.properties.flood_type}</p>
     <p></p>
-    <p class="info">Enquiry Time: ${feature.enquiry_time}</p>
+    <p class="info">Enquiry Time: ${feature.properties.enquiry_time}</p>
 
     </div>`
   layer.bindPopup(content)
