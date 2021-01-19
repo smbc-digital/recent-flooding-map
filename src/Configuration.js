dd286dfa25d262c57623a07b58dfafb52966f72a
Daniel Kinder <daniel.kinder@stockport.gov.uk>
2021-01-19 18:18:46 +0000

    styling spell check

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,6 +1,6 @@
 import Leaflet from 'leaflet'
 import { livefloodingincidentsPopup } from './Popups'
-import { livefloodingincidentsStyle } from './Styles'
+//import { livefloodingincidentsStyle } from './Styles'
 
 const Configuration = {
     Map: {
